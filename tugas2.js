d0dc4568275323510b0d8ab89760fd5d79ca475f
penerus programmer <engkoskostaman759@gmail.com>
2022-08-11 08:44:33 +0700

    tugas2js

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var angka = 25
+var name = "engkos kostaman"
+var array = [1, 2, 3, 4, 5]
+var istirahat = false
+var jam = 9
+if (jam > 6)
+    istirahat = false
+else
+    istirahat = true
+var kosong = "vitamin"
+
+var zonk
+
+console.log(angka)
+console.log(name)
+console.log(array)
+console.log(istirahat)
+console.log(kosong)
+console.log(zonk)
